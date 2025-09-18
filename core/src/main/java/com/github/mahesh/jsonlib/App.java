7f9d26be810c47408abbba3bfa3d99474603665e
Mahesh Sambaram <msambaram@lingo.com>
2025-09-18 22:38:04 +0530

    Initial commit

---
+++ core/src/main/java/com/github/mahesh/jsonlib/App.java
@@ -0,0 +1,13 @@
+package com.github.mahesh.jsonlib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
